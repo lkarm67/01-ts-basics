0cd8ff66772213f25c96a518dc4c5c16875d6c44
Luidmyla Karmeliuk <lkarm67@gmail.com>
2025-08-21 10:01:30 +0300

    import axios, solve task-8.ts

--- src/task-8.ts
+++ src/task-8.ts
@@ -1,8 +1,14 @@
 import axios from "axios";
 
-async function fetchPosts() {
-  const response = await axios.get(
-    '<https://jsonplaceholder.typicode.com/posts>'
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
+async function fetchPosts(): Promise<Post[]> {
+  const response = await axios.get<Post[]>(
+    'https://jsonplaceholder.typicode.com/posts'
   );
   return response.data;
 }
@@ -10,28 +16,3 @@ async function fetchPosts() {
 fetchPosts().then((posts) => {
   console.log(posts[0].title);
 });
-
-
-/*Завдання 8. HTTP-запити (файл task-8.ts)
-Функція fetchPosts робить GET-запит до API та повертає список постів.
-
-import axios from "axios";
-
-async function fetchPosts() {
-  const response = await axios.get(
-    '<https://jsonplaceholder.typicode.com/posts>'
-  );
-  return response.data;
-}
-
-fetchPosts().then((posts) => {
-  console.log(posts[0].title);
-});
-
-Завдання:
-Інсталюй бібліотеку axios командою npm i axios
-Створи інтерфейс Post, який описує об'єкт поста з такими полями:
-id: число
-title: рядок
-body: рядок
-3. Типізуй axios.get, щоб вказати, що API повертає масив постів.*/
